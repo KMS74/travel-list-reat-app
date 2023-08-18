f98c8a630d2ad7c7a63801b0ddf7f92761526f69
Karem Shabana <km425360@gmail.com>
2023-08-18 17:10:56 +0300

    Initial Commit 🚀

---
+++ src/components/Stats.js
@@ -0,0 +1,25 @@
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to you packing list 🚀</em>
+      </p>
+    );
+
+  const totalItems = items.length;
+  const totalPackedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((totalPackedItems / totalItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼You have ${totalItems} items on your list, and you already packed
+        ${totalPackedItems} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
+
+export default Stats;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//  exporting multiple React components from an index.js file
+// so we can easily import them collectively 
+export { default as Logo } from "./Logo";
+export { default as PackingList } from "./PackingList";
+export { default as Item } from "./Item";
+export { default as Form } from "./Form";
+export { default as Stats } from "./Stats";
